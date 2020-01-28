56531dc6d3e9ba9cee10a9b7e7aa3acfd0cc36cf
AnnaFinnerty <algfinnerty@gmail.com>
2020-01-27 19:16:13 -0700

    type functionality

--- public/js/input.js
+++ public/js/input.js
@@ -1,16 +1,22 @@
 class Input{
-    constructor(addToTextString){
+    constructor(addToTextString,nextLine,depressKey){
         //index of keyboard keys that create an action -- everything else gets passed to text
         this.addToTextString = addToTextString;
+        this.nextLine = nextLine;
+        this.depressKey = depressKey;
         this.timeSinceLastKeyStroke = 1000;
         this.specialKeys = {
-            'Enter' : true,
-            'Shift' : true,
-            'ArrowRight': null,
-            'ArrowLeft': null,
-            'ArrowUp': null,
-            'ArrowDown': null
+            'Enter' : this.enter,
+            'Shift' : this.shift,
+            'Space' : this.space,
+            'Tab': this.tab,
+            // 'Backspace': this.rerouteKeyVal,
+            'ArrowRight': this.rerouteKeyVal,
+            'ArrowLeft': this.rerouteKeyVal,
+            'ArrowUp': this.rerouteKeyVal,
+            'ArrowDown': this.rerouteKeyVal
         }
+        this.shift = false;
         this.awake();
     }
     awake(){
@@ -22,7 +28,7 @@ class Input{
         })
         for(let i = 0; i < activeKeys.length; i++){
             const key = this.typewriter.getElementById(activeKeys[i]+"-key");
-            console.log(key);
+            // console.log(key);
             key.addEventListener('click',(e)=>{
                 this.clickInput(e.target.id)
             })
@@ -36,8 +42,34 @@ class Input{
         console.log('user input: ' + key);
         if(this.specialKeys[key]){
             console.log('this is a special key!');
+            const f = this.specialKeys[key];
+            f(key)
         } else {
-            this.addToTextString(key,this.timeSinceLastKeyStroke);
+            if(this.shift){
+                this.addToTextString(key.toUpperCase(),this.timeSinceLastKeyStroke);
+                this.shift = false;
+            } else {
+                this.addToTextString(key,this.timeSinceLastKeyStroke);
+            }
         }
+    }
+    enter = () => {
+        this.nextLine();
+        this.depressKey('enter');
+    }
+    space = () => {
+        console.log('space')
+        this.addToTextString(" ",this.timeSinceLastKeyStroke);
+        this.depressKey('space');
+    }
+    tab = () => {
+        this.addToTextString("     ",this.timeSinceLastKeyStroke);
+        this.depressKey('tab');
+    }
+    shift = () => {
+       this.shift = true;
+    }
+    rerouteKeyVal = (key) => {
+        console.log('rerouting key val!' + key);
     }
 }
